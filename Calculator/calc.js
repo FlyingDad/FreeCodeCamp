6dd2a664ae00334239b0cddc315c138a1dc3b539
Michael <mkroth65@gmail.com>
2018-04-24 08:25:36 -0500

    Progress on calculator app - input working

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,7 +1,54 @@
+/* jshint esversion: 6 */
+
 $( document ).ready(function() {
 	console.log( "ready!" );
 
+let accumulator = 0;
+let inputReg = 0;
+let sign = true;   // true = pos, false: neg number
+let calcInProgress = false; // true = button pessed)except equal
+
+
 	$('.jqbtn').on('click', function(){
-		console.log($(this).attr('data-value'));
+		var input = $(this).attr('data-value');
+		//console.log('Press: ' + $(this).attr('data-value'));
+		display(input);
+		checkButtonType(input);
 	});
+
+	// Check button type, and perform action
+	function checkButtonType(input){
+		console.log("Check: " + input);
+		if(input == 'x' || input == '/' || input == '+' || input == '-') {
+			console.log('do math');
+			display('math');
+		} else if(input == 'ac' || input == 'ce') {
+			console.log('clear');
+			display('clear');
+		} else if(input == 'sign'){
+			console.log('change sign');
+		} else if (input == '.'){
+			console.log('decimal');
+		} else if (input == '='){
+			console.log('equal');
+		} else {
+			console.log('number');
+		}
+
+	}
+	// Calulation button pressed
+	
+
+	// Equal pressed
+	$('.equal').on('click', function() {
+		display('====');
+	});
+
+
+
+	function display(result){
+		//console.log(result);
+		$('.col-display h2').text(result);
+	}
+
 });
