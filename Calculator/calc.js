20aebf946a624082474851c89b0b4f56e429bd56
Michael <mkroth65@gmail.com>
2018-04-24 23:17:41 -0500

    Addition function working.

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -5,14 +5,17 @@ $( document ).ready(function() {
 
 let accumulator = 0;
 let inputReg = 0;
+let inputRegShift = 1;  // keep track of 10's
 let sign = true;   // true = pos, false: neg number
 let calcInProgress = false; // true = button pessed)except equal
+let operation = '';
 
+display(0);
 
 	$('.jqbtn').on('click', function(){
 		var input = $(this).attr('data-value');
 		//console.log('Press: ' + $(this).attr('data-value'));
-		display(input);
+		//display(input);
 		checkButtonType(input);
 	});
 
@@ -20,35 +23,83 @@ let calcInProgress = false; // true = button pessed)except equal
 	function checkButtonType(input){
 		console.log("Check: " + input);
 		if(input == 'x' || input == '/' || input == '+' || input == '-') {
-			console.log('do math');
-			display('math');
+			//console.log('do math');
+			//display('math');
+			performCalc(input);
 		} else if(input == 'ac' || input == 'ce') {
-			console.log('clear');
-			display('clear');
+			clear();
 		} else if(input == 'sign'){
 			console.log('change sign');
 		} else if (input == '.'){
 			console.log('decimal');
-		} else if (input == '='){
-			console.log('equal');
 		} else {
-			console.log('number');
+			numberPressed(input);
 		}
-
 	}
 	// Calulation button pressed
-	
+	function performCalc(calcType){
+		if(!calcInProgress){
+			console.log('no-calc', calcType);
+			accumulator = inputReg;
+			operation = calcType;
+			inputReg = 0;
+		} else {
+			switch(operation) {
+				case '+':
+				  operation = calcType;
+					console.log('adding');
+					accumulator += inputReg;
+					clearEntry();
+					display(accumulator);
+					break;
+			}
+		}
+		calcInProgress = true;
+	}
 
+	function equalPressed(){
+		console.log('equal: ' + operation)
+		performCalc(operation);
+	}
+	// All Clear pressed
+	function clear() {
+		accumulator = 0;
+		inputReg = 0;
+		inputRegShift = 1;
+		sign= true;
+		display(0);
+	}
+
+	function clearEntry() {
+		inputReg = 0;
+		inputRegShift = 1;
+		display(0);
+	}
+	// Clear Entry pressed
+	// Number pressed
+	function numberPressed(number){
+		//if first number pressed, put it in indexreg
+		let val = Number(number);
+		if(inputReg === 0){
+			inputReg += val;
+		} else {
+			//shift number left ( x 10)
+			inputReg *= 10;
+			inputReg += val;
+		}	
+		display(inputReg);
+	}
 	// Equal pressed
 	$('.equal').on('click', function() {
-		display('====');
+		equalPressed();
 	});
 
-
-
 	function display(result){
-		//console.log(result);
-		$('.col-display h2').text(result);
+		console.log('result: ' + result);
+		$('.col-display h2').text(result.toString());
+		$('#acc').text(accumulator);
+		$('#ireg').text(inputReg);
+		$('#ctype').text(operation);
 	}
 
 });
