f901b1a0fecf5690495d722403f7f100c23e20ac
Michael Kroth <mkroth65@gmail.com>
2018-11-21 22:24:23 -0600

    working on Codefights challenge

--- Codefights/almostIncreasingSequence.js
+++ Codefights/almostIncreasingSequence.js
@@ -1,24 +1,46 @@
-function almostIncreasingSequence(sequence) {
-	if(sequence.length <= 2) {return true};
-	for(let i = 0; i < sequence.length ; i++){
-		var sliced = sequence.slice();
-		sliced.splice(i, 1);
-		for(let j = 0; j < sliced.length - 1; j++ ){
-			if(sliced[j] >= sliced[j+1]){
-				break;
-			} else if(j === sliced.length - 2){
-				return true;
-			}			
-		}
-	}
+// function almostIncreasingSequence(sequence) {
+// 	if(sequence.length <= 2) {return true};
+// 	for(let i = 0; i < sequence.length ; i++){
+// 		var sliced = sequence.slice();
+// 		sliced.splice(i, 1);
+// 		for(let j = 0; j < sliced.length - 1; j++ ){
+// 			if(sliced[j] >= sliced[j+1]){
+// 				break;
+// 			} else if(j === sliced.length - 2){
+// 				return true;
+// 			}			
+// 		}
+// 	}
 
-	return false;
+// 	return false;
+// }
+
+function almostIncreasingSequence(sequence) {
+	let declines = 0;
+	for(let i = 0; i < sequence.length - 1; i++){
+			console.log('i: ', sequence[i], sequence[i+1])
+			if(sequence[i] > sequence [i+1]){
+				console.log('next less than')
+					if(i <= sequence.length - 2){
+							console.log('i less than sequence + 2')
+							if(sequence[i] != sequence [i+2]){
+									console.log('s2')
+									declines ++;
+							} else {
+							declines ++;
+					}
+					}
+			}
+			console.log(declines + '---------')
+	}
+	return declines <=1 ? true : false;
 }
 
-console.log(almostIncreasingSequence([1, 3, 2, 1])); // false
-console.log(almostIncreasingSequence([1, 3, 2])); // true
+
+//console.log(almostIncreasingSequence([1, 3, 2, 1])); // false
+//console.log(almostIncreasingSequence([1, 3, 2])); // true
 console.log(almostIncreasingSequence([1, 2, 1, 2])); // false
-console.log(almostIncreasingSequence([1, 1, 1, 2, 3])); //false
+//console.log(almostIncreasingSequence([1, 1, 1, 2, 3])); //false
 // Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
 
 // Example
