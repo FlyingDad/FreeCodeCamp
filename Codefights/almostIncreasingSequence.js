5da9c6c0e9ef25a485854d6df796baedb94349bb
Michael Kroth <mkroth65@gmail.com>
2018-12-06 16:19:39 -0600

    Completed almostIncreasingSequence, started chuck norris

--- Codefights/almostIncreasingSequence.js
+++ Codefights/almostIncreasingSequence.js
@@ -38,16 +38,39 @@
 
 
 //remove one at a time and check
+// function almostIncreasingSequence(sequence) {
+// 	for(let i = 0; i < sequence.length; i++){
+// 		let tempArr = sequence.slice();
+// 		tempArr.splice(i, 1);
+// 		if(checkIfAllIncreasing(tempArr) == true){
+// 			return true
+// 		}
+// 		// console.log(checkIfAllIncreasing(tempArr));
+// 	}
+// 	return false;
+// }
 function almostIncreasingSequence(sequence) {
-
+  var count = 0;
+    for (var i = 0; i < sequence.length; i++) {
+      if (sequence[i] <= sequence[i - 1]) {
+        count++;
+        if(sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1]) {
+          return false;
+        }
+      }
+    }
+    return count <= 1;
 }
 
 checkIfAllIncreasing = arr => {
-	for(let i = 0; i < array.length-1; i++){
-		if(arr[0] >= arr[i+1]){
+	
+	for(let i = 0; i < arr.length-1; i++){
+		// console.log(arr, arr[i], arr[i+1])
+		if(arr[i] >= arr[i+1]){
 			return false;
 		}
 	}
+	return true;
 }
 
 console.log(almostIncreasingSequence([1, 3, 2, 1])); // false
